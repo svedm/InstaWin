458ff2f0322350484169d755f1be382fb3c5eb42
Svetoslav Incognito <venom008@gmail.com>
2015-04-21 23:16:41 +0500

    Inject scripts & styles

--- InstaWin/InstaWin.Shared/injection-script.js
+++ InstaWin/InstaWin.Shared/injection-script.js
@@ -1,6 +1,16 @@
 ﻿// This script will be executed in the context of the webview
 var foo = function () {
-    console.log("Injection successful");
+	console.log("Injection successful");
 };
 
 foo();
+
+$(document).ready(function () {
+	console.log("ready!");
+	if (document.location.pathname !== "/") {
+		//on register page
+	} else {
+		//on main page
+		console.log("fuck");
+	}
+});
